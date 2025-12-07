491147bac8de8157db35e79ebcbd6d187661c31b
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2025-12-07 14:40:38 -0300

    EDGE Crypto Suite: Official Graphical User Interface v1

--- gui/app.tcl
+++ gui/app.tcl
@@ -1,4 +1,19 @@
 #!/usr/bin/wish
+###############################################################################
+#   EDGE Crypto Suite -- Pure Tcl/Tk Graphical Cryptographic Toolkit          #
+#   Copyright (C) 2020-2025 Pedro F. Albanese <pedroalbanese@hotmail.com>     #
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify it   #
+#   under the terms of the ISC License.                                       #
+#                                                                             #
+#   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  #
+#   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF          #
+#   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR   #
+#   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    #
+#   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN     #
+#   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF   #
+#   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.            #
+###############################################################################
 
 # Style settings
 set bg_color "#f5f5f5"
